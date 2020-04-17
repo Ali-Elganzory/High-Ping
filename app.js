04be4556b7dc573915f91d364c2af1c8df59887a
Ali Elganzory <50919012+Ali-Elganzory@users.noreply.github.com>
2020-04-17 21:47:54 +0200

    general mods

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const http = require("http");
-const fs = require("fs");
+const path = require("path");
+// const spawn = require("child_process").spawn;
 
-function requestHandler(req, res) {
-    
-}
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer(requestHandler);
+const homepageRoutes = require("./routes/homepage");
 
-server.listen(3000);
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+// Home page
+app.use(homepageRoutes);
+
+// invalid page
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
+
+/* const pythonProcess = spawn("python", [
+  path.join("python", "test.py"),
+  5,
+  12,
+]);
+pythonProcess.stdout.on("data", (data) => {
+  res.send(`<h1>${data}</h1>`);
+}); */
---
+++ python/test.py
@@ -0,0 +1,5 @@
+import sys;
+
+print(sys.argv[1] + sys.argv[2])
+
+sys.stdout.flush()
